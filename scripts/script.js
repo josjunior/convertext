e25ff55c9d6f7d750c91883388e471cbdd8c392b
José Júnior <jredit7@gmail.com>
2022-12-10 09:14:10 -0300

    Inicia o projeto

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+function convUpper() {
+  let textUpper = document.getElementById('text_area').style.textTransform = 'uppercase';
+}
+function convLower() {
+  let textLower = document.getElementById('text_area').style.textTransform = 'lowercase';
+}
+function convCapi() {
+    let textCapi = document.getElementById('text_area').style.textTransform = 'capitalize';
+}
+function reload() {
+    let textReload = window.location.reload();
+}
