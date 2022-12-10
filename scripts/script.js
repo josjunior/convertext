07edc3b78c9a0116bd278b07aaa46489f0db855c
José Júnior <jredit7@gmail.com>
2022-12-10 13:18:41 -0300

    Versão 1

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+// Text Transform
 function convUpper() {
   let textUpper = document.getElementById('text_area').style.textTransform = 'uppercase';
 }
@@ -5,8 +6,27 @@ function convLower() {
   let textLower = document.getElementById('text_area').style.textTransform = 'lowercase';
 }
 function convCapi() {
-    let textCapi = document.getElementById('text_area').style.textTransform = 'capitalize';
+  let textCapi = document.getElementById('text_area').style.textTransform = 'capitalize';
 }
-function reload() {
-    let textReload = window.location.reload();
+
+// Text Decoration 
+function overLine() {
+  let textOverline = document.getElementById('text_area').style.textDecoration = 'overline';
+}
+function underLine() {
+  let textUnderline = document.getElementById('text_area').style.textDecoration = 'underline';
+}
+function lineThrough() {
+  let textLineThrough = document.getElementById('text_area').style.textDecoration = 'line-through';
+}
+
+// Text Style 
+function bold() {
+  let textBold = document.getElementById('text_area').style.fontWeight = 'bold';
+}
+function italic() {
+  let textItalic = document.getElementById('text_area').style.fontStyle = 'italic';
+}
+function oblique() {
+  let textOblique = document.getElementById('text_area').style.fontStyle = 'oblique';
 }
